4bdb06e217f6182593bee2423f86998a61ebf1a0
qiaoyuang <qiaoyuang2012@gmail.com>
2026-05-16 13:49:51 +0100

    Back to Android target 36

--- mmkv-kotlin/build.gradle.kts
+++ mmkv-kotlin/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
     jvmToolchain(21)
     androidLibrary {
         namespace = "com.ctrip.flight.mmkv"
-        compileSdk = 37
+        compileSdk = 36
         minSdk = 23
     }
     iosArm64()
